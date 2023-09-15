e25017d77f8abd9b30e7a797d57306ed5d88ec96
gasp8918 <gasp8918@student.acitech.org>
2023-09-15 09:45:12 -0400

    DO EVERYTHING

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var s = 0
-
-for (let i = 1; i < 1000; i++) {
-  if (i % 3 == 0) {
-    s += i;
+function validateForm() {
+  var fName = document.forms['form']['fname'].value; // we use this to grab the value from the form. since this isn't a real log in we don't really care about half the stuff
+  var pw = document.forms['form']['pw'].value;
+  var cpw = document.forms['form']['cpw'].value;
+  if (fName.match(/\d+/g)){ // matches the first name with any digits and if its returns true, don't let the user get through
+    alert('can youj follow basic instructions freak')
+    return false;
+  } 
+  else if (!(pw.match(cpw)) || cpw == "") { // pretty obvious, makes sure the password is the same as the confirm password field
+    alert("Incorrect Confirmed Password");
   }
-  else if(i % 5 == 0)
-  {
-    s += i;
+  else {
+    alert("you win!")
   }
 }
-
-console.log(s)
